1cc3339208745c7dc1d0ad7149f4246a6e81753d
Бердыев Бары <bary.berdiyev.97@mail.ru>
2023-10-17 14:49:51 +0700

    мелкие исправления

--- src/entities/Card/ui/Card.tsx
+++ src/entities/Card/ui/Card.tsx
@@ -2,6 +2,7 @@ import { Button } from 'antd';
 import { motion, useReducedMotion } from 'framer-motion';
 import { FC, memo } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { getImageName } from 'shared/model/lib';
 import styles from './Card.module.scss';
 import { CardProps } from './Card.props';
 import CloseIcon from './icons/close.svg';
@@ -9,6 +10,8 @@ import CloseIcon from './icons/close.svg';
 const Card: FC<CardProps> = memo(({ card, handleClickCard, handleClose }) => {
 	const API_URL = process.env.API_URL;
 	const shouldReduceMotion = useReducedMotion();
+
+	const name = getImageName(card);
 
 	return (
 		<motion.div
@@ -23,7 +26,7 @@ const Card: FC<CardProps> = memo(({ card, handleClickCard, handleClose }) => {
 				className={styles.image}
 			/>
 			<div className={styles.content} onClick={() => handleClickCard(card)}>
-				<h5>{card.image.replace(`${card.category}/`, '')}</h5>
+				<h5>{name}</h5>
 			</div>
 			<Button
 				className={styles.closeButton}
